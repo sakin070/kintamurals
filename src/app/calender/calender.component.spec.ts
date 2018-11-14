1b5de317f0d2e0de7e251f7cf7883f1e7b890f5b
Hasan Baig <hbaig@kinaxis.com>
2018-11-14 16:30:03 -0500

    UI

--- src/app/calender/calender.component.spec.ts
+++ src/app/calender/calender.component.spec.ts
@@ -1,6 +1,8 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CalenderComponent } from './calender.component';
+import { InputObject, TextBox } from  '@syncfusion/ej2-inputs';
+import { Button } from '@syncfusion/ej2-buttons';
 
 describe('CalenderComponent', () => {
   let component: CalenderComponent;
@@ -22,4 +24,26 @@ describe('CalenderComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  let inputobj1: TextBox = new TextBox({
+      placeholder: 'Admin 1',
+      floatLabelType: 'Auto'
+  });
+  inputobj1.appendTo(#Admin1);
+
+  let button: Button = new Button();
+  button.appendTo('#normalbtn');
+
+
+
+
 });
+/**
+  let customers: DropDownList = new DropDownList({
+    dataSource:new DataManager({ url:'http://js.syncfusion.com/demos/ejServices/Wcf/Northwind.svc/' }),
+    query: new Query().from('Customers').select(['ContactName', 'CustomerID']).take(5),
+    fields: { text: 'ContactName', value: 'CustomerID' },
+    placeholder: 'Select a customer'
+  });
+
+customers.appendTo("#list");*/
--- src/app/calender/calender.component.ts
+++ src/app/calender/calender.component.ts
@@ -1,17 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { extend } from '@syncfusion/ej2-base';
-import { EventSettingsModel, DayService, WeekService, WorkWeekService, MonthService,
-  AgendaService, ResizeService, DragAndDropService } from '@syncfusion/ej2-angular-schedule';
+import { EventSettingsModel, WeekService, MonthService,
+  ResizeService, DragAndDropService } from '@syncfusion/ej2-angular-schedule';
 import { scheduleData } from '../datasource';
 
 @Component({
   selector: 'app-calender',
   templateUrl: './calender.component.html',
   styleUrls: ['./calender.component.scss'],
-  providers: [DayService, WeekService, WorkWeekService, MonthService, AgendaService, ResizeService, DragAndDropService]
+  providers: [WeekService, MonthService, ResizeService, DragAndDropService]
 })
 export class CalenderComponent implements OnInit {
-  public selectedDate: Date = new Date(2018, 1, 15);
+  public selectedDate: Date = new Date(2018, 1, 20);
   public eventSettings: EventSettingsModel = { dataSource: <Object[]>extend([], scheduleData, null, true) };
 
   constructor() { }
